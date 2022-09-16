f7ba204dee3b72364a22597a58e783d412f0929b
KovalevEgor14 <olegnorilsk77@gmail.com>
2022-09-17 00:54:04 +0500

    first

--- app/src/main/java/ru/timcock/petkt/MainActivity.kt
+++ app/src/main/java/ru/timcock/petkt/MainActivity.kt
@@ -2,10 +2,34 @@ package ru.timcock.petkt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yandex.mapkit.Animation
+import com.yandex.mapkit.MapKitFactory
+import com.yandex.mapkit.geometry.Point
+import com.yandex.mapkit.map.CameraPosition
+import ru.timcock.petkt.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        MapKitFactory.setApiKey("a17efda7-6382-40d5-abd6-a38cc0ab6287")
+        MapKitFactory.initialize(this)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.mapView.map.move(CameraPosition(Point(55.751574, 37.573856), 11.0f, 0.0f, 0.0f), Animation(Animation.Type.SMOOTH, 0.0f), null)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        MapKitFactory.getInstance().onStart()
+        binding.mapView.onStart()
+    }
+
+    override fun onStop() {
+        binding.mapView.onStop()
+        MapKitFactory.getInstance().onStop()
+        super.onStop()
     }
 }
